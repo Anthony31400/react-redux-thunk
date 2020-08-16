f39cc792e0fbcc6204f27080d1165752de33f826
Anthony <anthony@MacBook-Pro-de-Anthony.local>
2020-08-16 15:59:12 +0200

    refracto redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
 import * as serviceWorker from './serviceWorker';
+import AppContainer from './App.container';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import rootReducer from './reducer';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <AppContainer />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
